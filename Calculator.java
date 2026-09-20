14c5dda6cf86e117ab655a5f9b664b4b8991cbc0
elenaanghel <andreea.comanescu35@gmail.com>
2026-09-20 13:58:07 +0300

    fix

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,13 @@
-
 public class Calculator {
-        //Calculeaza suma a doua numere
-public int aduna(int a, int b){
+    //Calculeaza suma a doua numere
+    public int aduna(int a, int b) {
         //Temporar, adunam si un bonus harcodat
-        int bonus=2;
-        return a+b+bonus;
-        }
-        public static void main(String[] args) {
-    Calculator calc = new Calculator();
-    System.out.println("Rezultat: " + calc.aduna(5, 10));
-        }
-        }
+        int bonus = 2;
+        return a + b + bonus;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Rezultat: " + calc.aduna(5, 10));
+    }
+}
